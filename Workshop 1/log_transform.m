81c0b917cde1f3e271ae6e73081d4da843945dc2
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-10-01 15:10:07 +0200

    added comments

--- Workshop 1/log_transform.m
+++ Workshop 1/log_transform.m
@@ -1,17 +1,26 @@
 function B= log_transform(A,c)
 %Denne funktion anvender intensitetstransformation c*log(1+a_ij) på
 %matricen A.
-%
-%error('Denne funktion er endnu ikke implementeret')
+
+
+% vi sikkeres os at alle tallene i billedet er doubles
 ad= im2double(A);
+
+% vi gemmer billede sørelsen i variablerne xs og ys (står for x size og y size)
 [xs,ys] = size(A);
+% vi skriver dem ud i konsolen
 disp(size(A))
+%vi gemmer billede værdierne i x 
 x= ad;
+
+%vi går igennem alle pixel og udføre log tranformationen
 for i = 1:xs
     for j = 1:ys
        x(i,j) = c * log(1+ad(i,j)); 
     end
 end
+
+%vi sætter b til x for at sende vores behandlede blide ud af funktion
 B = x;
 end
 
